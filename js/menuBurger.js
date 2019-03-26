53c6e05d380fff2d11ff3829f966a02acff893d4
Rachel Boulay <boulayrachel.52@gmail.com>
2019-03-26 00:35:00 -0400

    jen ai rien a chier

--- js/menuBurger.js
+++ js/menuBurger.js
@@ -2,6 +2,13 @@
     var elmLi = document.getElementsByTagName("li");
     var elmLiLength = elmLi.length;
     var elmMain = document.getElementsByTagName("main")[0];
+    var menuCourt = ["Acc", "Comp", "Exp", "Pro"];
+    var menuLong = ["Accueil", "Compétences", "Expérience", "Projets"];
+
+    petitEcran();
+
+    window.addEventListener("resize", petitEcran);
+         
 
     for(var i = 0; i < elmLiLength; i++){
         elmLi[i].addEventListener("click", translateMenu);
@@ -13,5 +20,17 @@
         }
         elmMain.style.transform = "translateX(-" + e.target.getAttribute("data-index") + "%)";
         e.target.style.textShadow = "1px 0px 0px rgb(255, 28, 244)";
+    }
+
+    function petitEcran(){
+        if(window.innerWidth < 720){
+            for(var i = 0; i < elmLiLength; i++){
+                elmLi[i].innerHTML = menuCourt[i];
+            }
+        } else{
+            for(var i = 0; i < elmLiLength; i++){
+                elmLi[i].innerHTML = menuLong[i];
+            }
+        }
     }
 })()
